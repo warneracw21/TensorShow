328a3e98ee8ccd441e6d280a8382518abba98922
Andrew Warner <warneracw@gmail.com>
2020-04-03 15:52:06 -0400

    Updates

---
+++ precache-manifest.57023fbdce98f8e95e6e903998736ef2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfbf422ba8a99f7f18ca90a844a59c43",
+    "url": "/TensorShow/index.html"
+  },
+  {
+    "revision": "44dbd4f5f8eb052bffb8",
+    "url": "/TensorShow/static/js/2.bd5525a0.chunk.js"
+  },
+  {
+    "revision": "5f5ca9dccbfa3236d2de99c480d68e94",
+    "url": "/TensorShow/static/js/2.bd5525a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37b951b7c5a894df3f5f",
+    "url": "/TensorShow/static/js/main.cb96476a.chunk.js"
+  },
+  {
+    "revision": "d882f45532865d123d4d",
+    "url": "/TensorShow/static/js/runtime-main.63eb1d50.js"
+  }
+]);
--- tensor-show/src/constants.js
+++
@@ -1,4 +0,0 @@
-// Layer Type Constants
-const CONV = 'convolution_layer';
-const POOL = 'pooling_layer';
-const FULL = 'full_layer';
