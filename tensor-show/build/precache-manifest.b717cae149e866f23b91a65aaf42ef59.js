3f4663f4c2ad17ab2f151d93a5fb06d6084209af
Andrew Warner <warneracw@gmail.com>
2020-05-06 13:54:16 -0400

    adding build

---
+++ tensor-show/build/precache-manifest.b717cae149e866f23b91a65aaf42ef59.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1f6bc3aefc88b06ce17312a769884fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d83b828daa339c931cd9",
+    "url": "/static/js/2.672a8aad.chunk.js"
+  },
+  {
+    "revision": "3be25bf51018a491ef7f73dfd2dd74c4",
+    "url": "/static/js/2.672a8aad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f04615d981d9c38d236c",
+    "url": "/static/js/main.bb8c1302.chunk.js"
+  },
+  {
+    "revision": "8f95abaab610a80c1f9f",
+    "url": "/static/js/runtime-main.1bf9e53a.js"
+  }
+]);
